5280152d48f23e76c8318fca3f97a125349b19ba
qusaiqishta <qeshtaqusai0@gmail.com>
2021-04-02 17:48:20 +0300

    fix lab8

--- server.js
+++ server.js
@@ -2,10 +2,9 @@
 
 require('dotenv').config();
 
-
 const express = require('express');
 const cors = require('cors');
-
+const pg = require('pg');
 const superagent = require('superagent');
 
 const PORT = process.env.PORT;
@@ -14,46 +13,29 @@ app.use(cors());
 const GEO_CODE_API_KEY = process.env.GEO_CODE_API_KEY;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const PARK_API_KEY=process.env.PARK_API_KEY;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 
+// Database Connection Setup
+const client = new pg.Client({
+  connectionString: DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 
 //routes
-app.get('/location', handleLocationrequest);
+app.get('/location', LocationDatabase);
 app.get('/weather', handleWeatherrequest);
 app.get('/parks',handleParkRequest)
 app.use('*', notFoundHandler);
 
 
-function handleLocationrequest(request, response) {
-    const city = request.query.city;
-    console.log('ddddddddddd')
-    const url = `https://us1.locationiq.com/v1/search.php?key=${GEO_CODE_API_KEY}&q=${city}&format=json`;
-
-    const cityQueryParam = {
-        key: GEO_CODE_API_KEY,
-        city: city,
-        format: 'json'
-    };
-
-    superagent.get(url).then(resData => {
-        let location = new Location(city, resData.body[0])
-        response.status(200).send(location);
-        }).catch((error) => {
-            
-            response.status(500).send('Sorry, something went wrong');
-        });
-    
-    
-        if (!city) {
-            response.status(404).send('no search query was provided');
-        }
-
-    }
 
 function handleWeatherrequest(request, response) {
     let weatherArray = [];
-    const city =request.query.search_query;
+    const city =request.query.city;
     const longitude = request.query.longitude;
     const latitude = request.query.latitude;
     const url=`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${latitude}&lon=${longitude}&key=${WEATHER_API_KEY}`;
@@ -70,7 +52,6 @@ function handleWeatherrequest(request, response) {
 
 
 
-
 function Location(city, data) {
     this.formatted_query = data.display_name;
     this.latitude = data.lat;
@@ -81,21 +62,45 @@ function Location(city, data) {
 
 
 function Weather(value) {
-    this.forecast = value.weather.description;
-    this.dateTime = value.dateTime;
+  this.forecast = value.weather.description;
+  this.dateTime = value.dateTime;
 }
 function Parks(data){
     this.name=data.name;
-    this.fee=data.fee;
+    this.fee=data.fees||'0.00';
     this.description=data.description;
     this.url=data.url;
-    this.address=data.address;
+    this.address=data.addresses;
 
+}
+
+function LocationDatabase(request,response){
+  const city = request.query.city;
+  let selectLocation = 'SELECT * FROM location WHERE search_query = $1;';
+  let safeValuesSelect = [city];
+  client.query(selectLocation, safeValuesSelect).then(result => {
+    if (result.rows.length > 0) {
+      response.status(200).json(result.rows);
+    }
+    else {
+      const url = `https://us1.locationiq.com/v1/search.php?key=${GEO_CODE_API_KEY}&q=${city}&format=json`;
+      superagent.get(url).then(data => {
+      let location = new Location(city, data.body[0]);
+        const newLocation = 'INSERT INTO location (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);';
+        const safeValuesInsert = [city, location.formatted_query, location.latitude, location.longitude];
+        client.query(newLocation, safeValuesInsert).then(result => {
+          response.status(200).json(location);
+        }).catch(() => {
+          response.status(500).send('Something Went Wrong');
+        })
+      })
+    }
+  })
 }
 
 
 function handleParkRequest(request,response){
-    const parks=[];
+    let parks=[];
 
     const longitude = request.query.longitude;
     const latitude = request.query.latitude;
@@ -104,11 +109,11 @@ superagent.get(url).then(resData => {
     parks = resData.body.data.map((value, index) => {
       return (new Parks(value));
     });
+    response.json(parks);
   }).catch(() => {
       response.status(500).send('Something Went Wrong');
     })
 }
-
 
 
 function errorMsg(response, data) {
@@ -120,14 +125,18 @@ function errorMsg(response, data) {
     }
 }
 
-
 app.use('*', (req, res) => {
     res.status(404).send('The Route not found');
 });
-app.listen(PORT, () => console.log(`Listening to Port ${PORT}`));
-
+client.connect().then(() => {
+    app.listen(PORT, () => {
+      console.log("Connected to database:", client.connectionParameters.database) //show what database we connected to
+      console.log('Server up on', PORT);
+    });
+  })
 
 
 function notFoundHandler(request, response) {
     response.status(404).send('huh?');
 }
+
